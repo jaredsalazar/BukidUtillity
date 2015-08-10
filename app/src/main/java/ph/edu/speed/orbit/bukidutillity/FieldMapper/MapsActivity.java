40c84ef36a8e2e90330857d2cb7ddf8f243f9f84
lexelouse05 <jj_salazar05@hotmail.com>
2015-08-11 01:25:38 +0800

    Now using GeoJSON

--- app/src/main/java/ph/edu/speed/orbit/bukidutillity/FieldMapper/MapsActivity.java
+++ app/src/main/java/ph/edu/speed/orbit/bukidutillity/FieldMapper/MapsActivity.java
@@ -11,25 +11,31 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolygonOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import ph.edu.speed.orbit.bukidutillity.R;
 
-
 public class MapsActivity extends FragmentActivity {
 
-    Integer Count;
-    Double[] lat = new Double[99];
-    Double[] lng = new Double[99];
+    int Count;
+    double[] lat;
+    double[] lng;
     SharedPreferences pref;
+    JSONObject[] FarmPlotObjects;
+    JSONArray FarmArray, PlotArray;
+    JSONObject plotpolygonArray;
+    String[] plotpolygonArrayString, plotpolygonAreaString;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
@@ -80,59 +86,59 @@ public class MapsActivity extends FragmentActivity {
     //Setting up the polygons if available from sharedpref
     private void viewmap() {
 
-        pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        String info = pref.getString("Area","No information");
+        pref = getApplicationContext().getSharedPreferences("MyFarm", MODE_PRIVATE);
         TextView stat = (TextView) findViewById(R.id.tvMapStat);
-        stat.setText("Area: " + info);
 
-        String str = pref.getString("Coordinates", "[]");
-        int num = pref.getInt("Count", 0);
-        new json_parser.coordinate_parser(str,num);
+        String str = pref.getString("feature", "[]");
 
-        lat = json_parser.Lat;
-        lng = json_parser.Lng;
+        if (!(str == "[]")) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            JSONObject feature = null;
+            try {
+                feature = new JSONObject(str);
+                JSONObject geometry = feature.getJSONObject("geometry");
+                JSONArray coordinates = geometry.getJSONArray("coordinates");
+                JSONArray inner_coordinates = coordinates.getJSONArray(0);
 
+                double[] lat = new double[inner_coordinates.length()];
+                double[] lng = new double[inner_coordinates.length()];
 
-        /** test coordinates for polygon simulation */
-        //lng = new Double[]{120.53849458694458,120.53875207901,120.53907394409178,120.53912758827211,120.53921341896057,120.53922414779663,120.53929924964905,120.53935289382935,120.53918123245239,120.53899884223937,120.53887009620665,120.53875207901,120.53848385810853,120.53849458694458};
-        //lat = new Double[]{14.68288638982858,14.682969417571767,14.683052445283455,14.682709955769598,14.682429736677431,14.682294816245724,14.682108003202861,14.681869297415833, 14.681807026298122,14.681827783339322, 14.681848540378576,14.682056110662632,14.682626927927863,14.682844875945142};
+                for (int i = 0; i < inner_coordinates.length(); i++) {
+                    JSONArray point = inner_coordinates.getJSONArray(i);
+                    lat[i] = point.getDouble(1);
+                    lng[i] = point.getDouble(0);
+                }
 
-        Count = lat.length;
+                JSONObject properties = feature.getJSONObject("properties");
+                String plotname = properties.getString("plotname");
+                double area = properties.getDouble("Area");
 
-        if (lng.length != 0){
-            setUpMap();
-        }else{
-            Toast.makeText(getApplicationContext(), "coordinates are null", Toast.LENGTH_SHORT).show();
+                setUpMap(lat, lng, inner_coordinates.length(), plotname, area);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
 
-
     //Setting up the polygons on the map
-    private void setUpMap() {
-        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-        //Setting up if ok
-        if((mMap != null) && (lat != null) && (lng != null) && Count != 0){
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-            PolygonOptions setup = new PolygonOptions().strokeColor(Color.RED).fillColor(Color.BLUE);
-
-            for(int i = 0; i < Count; i++){
-                setup.add(new LatLng(lat[i], lng[i]));
-            }
-
-            Polygon polygon = mMap.addPolygon(setup);
-
-            //zoom to location
-            LatLng coordinate = new LatLng(lat[0], lng[0]);
-            CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate,17);
-            mMap.animateCamera(yourLocation);
-        }else{
-            Toast.makeText(getApplicationContext(), "no polygons to show", Toast.LENGTH_SHORT).show();
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-
-
+    private void setUpMap(double[] lat, double[] lng, int length, String plotname, double area) {
+        PolygonOptions setup = new PolygonOptions().strokeColor(Color.RED).fillColor(Color.BLUE);
+        for (int i = 0; i < length; i++) {
+            setup.add(new LatLng(lat[i], lng[i]));
         }
+
+        MarkerOptions options = new MarkerOptions().position(new LatLng(lat[0], lng[0]));
+        options.title("Plotname: " + plotname);
+        options.snippet("Area: " + area + " Sq-m");
+        mMap.addMarker(options);
+        mMap.addPolygon(setup);
+
+        //zoom to location
+        LatLng coordinate = new LatLng(lat[0], lng[0]);
+        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 20);
+        mMap.animateCamera(yourLocation);
 
     }
 
